42cc1a9a3b2f79c1452625ac0df4a58e8e188a43
joehurdles <joeenocent@gmail.com>
2022-04-04 18:52:09 -0700

    colors changed

--- lib/helpers/color_helper.dart
+++ lib/helpers/color_helper.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 Color getColor(BuildContext context, double percent) {
   if (percent>= 0.50) {
-    return Colors.red;
+    return Theme.of(context).primaryColor;
   } else if (percent >= 0.25) {
     return Colors.orange;
   }
-  return Theme.of(context).primaryColor;
+  return Colors.red;
 }
